c9c0371dcf182dec704b3459dd3a3961d20af9e5
Ivan Pereira <ivan@ivansweb.com.br>
2023-08-29 04:43:15 +0100

    seed the events table with subscriber, followers, donations and merch sales informations

--- database/seeders/EventsTableSeeder.php
+++ database/seeders/EventsTableSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Donation;
 use App\Models\Event;
+use App\Models\Follower;
+use App\Models\MerchSale;
 use App\Models\Subscriber;
 use Illuminate\Database\Seeder;
 
@@ -13,7 +16,11 @@ class EventsTableSeeder extends Seeder
      */
     public function run(): void
     {
-        $subscribers = Subscriber::select(
+
+        /*
+         * Subscribers
+         */
+        $rows = Subscriber::select(
             'users1.name as user_name',
             'users2.name as subscriber_name',
             'subscribers.created_at',
@@ -23,16 +30,91 @@ class EventsTableSeeder extends Seeder
         )
             ->join('users as users1', 'users1.id', '=', 'subscribers.user_id')
             ->join('users as users2', 'users2.id', '=', 'subscribers.subscriber_id')
+            ->limit(10)
             ->get();
 
 
-        foreach ($subscribers as $subscriber) {
-            $message = "{$subscriber->subscriber_name} ({$subscriber->subscription_level}) subscribed to you!";
+        foreach ($rows as $row) {
+            $message = "{$row->subscriber_name} ({$row->subscription_level}) subscribed to you!";
             Event::create([
                 'message' => $message,
                 'read' => false,
-                'created_at' => $subscriber->created_at,
-                'user_id' => $subscriber->user_id,
+                'created_at' => $row->created_at,
+                'user_id' => $row->user_id,
+            ]);
+        }
+
+        /*
+         * Followers
+         */
+        $rows = Follower::select(
+            'users1.name as user_name',
+            'users2.name as follower_name',
+            'followers.created_at',
+            'followers.user_id',
+            'followers.follower_id',
+        )
+            ->join('users as users1', 'users1.id', '=', 'followers.user_id')
+            ->join('users as users2', 'users2.id', '=', 'followers.follower_id')
+            ->limit(10)
+            ->get();
+
+
+        foreach ($rows as $row) {
+            $message = "{$row->follower_name} followed you!";
+            Event::create([
+                'message' => $message,
+                'read' => false,
+                'created_at' => $row->created_at,
+                'user_id' => $row->user_id,
+            ]);
+        }
+
+        /*
+         * Donations
+         */
+        $rows = Donation::select(
+            'users1.name as user_name',
+            'users2.name as donor_name',
+            'donations.*',
+        )
+            ->join('users as users1', 'users1.id', '=', 'donations.user_id')
+            ->join('users as users2', 'users2.id', '=', 'donations.donor_id')
+            ->limit(10)
+            ->get();
+
+
+        foreach ($rows as $row) {
+            $message = "{$row->donor_name} donated {$row->amount} {$row->currency} to you! \"({$row->donation_message})\" ";
+            Event::create([
+                'message' => $message,
+                'read' => false,
+                'created_at' => $row->created_at,
+                'user_id' => $row->user_id,
+            ]);
+        }
+
+        /*
+         * Merch Sales
+         */
+        $rows = MerchSale::select(
+            'users1.name as user_name',
+            'users2.name as buyer_name',
+            'merch_sales.*',
+        )
+            ->join('users as users1', 'users1.id', '=', 'merch_sales.user_id')
+            ->join('users as users2', 'users2.id', '=', 'merch_sales.buyer_id')
+            ->limit(10)
+            ->get();
+
+
+        foreach ($rows as $row) {
+            $message = "{$row->buyer_name} bought {$row->amount} fancy {$row->item_name} from you for {$row->price} USD! ";
+            Event::create([
+                'message' => $message,
+                'read' => false,
+                'created_at' => $row->created_at,
+                'user_id' => $row->user_id,
             ]);
         }
     }
